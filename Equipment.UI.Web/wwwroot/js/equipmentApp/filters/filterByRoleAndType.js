ff54505ab3579a25404165c5917bd698e08d03b0
Mickael <MA>
2020-02-21 05:26:08 +0300

    Добавил фильтр по списку выделенных ролей и типов;
    Добавил отображение числа отфильтрованных единиц оборудования

---
+++ Equipment.UI.Web/wwwroot/js/equipmentApp/filters/filterByRoleAndType.js
@@ -0,0 +1,19 @@
+﻿app
+
+.filter("filterByRoleAndType", function(){
+   return function(equipment, roles, types){
+       roles = roles || [];
+       types = types || [];
+       
+       if(roles.length || types.length){
+           var filtered = equipment.filter(function (eq) {
+               return roles.contains(eq.role) || types.contains(eq.type);
+           });
+           
+           return filtered;
+       }
+       else {
+           return equipment;
+       }
+   } 
+});
